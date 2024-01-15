e12e940ef9bd4b4776a073e0eef85eb4983e3f41
Roushan Kumar Singh <119677092+Roushankumarsingh@users.noreply.github.com>
2024-01-15 12:16:33 +0530

    Add files via upload

---
+++ currencyconverter.js
@@ -0,0 +1,60 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/" ; 
+
+const dropdowns = document.querySelectorAll(".dropdown select") ; 
+const fromCurr = document.querySelector(".from select") ; 
+const toCurr = document.querySelector(".to select") ; 
+
+// for(let currCode in countryList)
+// {
+//     console.log(currCode,countryList[currCode]) ; 
+// }
+for(let select of dropdowns)
+{
+    for(let currCode in countryList)
+    {
+        let newOption = document.createElement("option") ; 
+        newOption.innerText = currCode ; 
+        newOption.value = currCode ; 
+        if(select.name == "from" && currCode == "USD")
+        {
+            newOption.selected = "selected" ; 
+        }
+        else if(select.name == "To" && currCode == "INR")
+        {
+            newOption.selected = "selected" ; 
+        }
+        select.append(newOption) ;
+        select.addEventListener("change",(evt) =>
+        {
+            updateFlag(evt.target) ; 
+        }) 
+    }
+}
+const updateFlag = (element) =>
+{
+    let currCode = element.value; 
+    let countryCode = countryList[currCode] ; 
+    newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png` ; 
+    let img = element.parentElement.querySelector("img") ; 
+    img.src = newSrc ; 
+}
+const msg = document.querySelector(".msg") ; 
+let btn = document.querySelector("button") ; 
+btn.addEventListener("click" ,async (evt) =>
+{
+    evt.preventDefault() ; 
+    let amount = document.querySelector("form input") ; 
+    let amtVal = amount.value ; 
+    if(amtVal == "" || amtVal <1)
+    {
+        amtVal = 1 ; 
+        amount.value = "1" ; 
+    }
+    // console.log(fromCurr,toCurr) ; 
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json` ;
+    let response = await fetch(URL)  ; 
+    let data = await response.json() ; 
+    let rate = data[toCurr.value.toLowerCase()] ; 
+    let finalAmt = amtVal * rate ; 
+    msg.innerText = `${amtVal} ${fromCurr.value}  = ${finalAmt} ${toCurr.value} ` ; 
+})
